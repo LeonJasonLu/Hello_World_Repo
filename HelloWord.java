d745739c1437a8e24783ab9fbe8a2e3117b05af5
LeonJasonLu <jason1997.0102@gmail.com>
2023-05-14 21:19:06 +0800

    Update HelloWord.java

--- HelloWord.java
+++ HelloWord.java
@@ -1,5 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World & Github!");
+        System.out.println("Hello, another commit~");
     }
 }
